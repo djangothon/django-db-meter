90374b7df197409ad6c8136afec02a5a4ef7ffa0
Virendra Jain <virendra@hackerearth.com>
2015-08-23 06:55:49 +0530

    add views and urls for showing aggregated data

--- django_db_meter/urls.py
+++ django_db_meter/urls.py
@@ -2,8 +2,44 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^app-wise-data/(?P<app_name>)\w+/$',
+    url(r'^$',
+        'django_db_meter.views.index',
+        name='index'
+    ),
+    url(r'^app-wise-data/(?P<app_name>\w+)/$',
         'django_db_meter.views.get_app_wise_data',
         name='app_wise_data'
-    )
+    ),
+    url(r'^table-wise-data/(?P<table_name>\w+)/$',
+        'django_db_meter.views.get_table_wise_data',
+        name='table_wise_data'
+    ),
+    url(r'^db-wise-data/(?P<db_name>\w+)/$',
+        'django_db_meter.views.get_db_wise_data',
+        name='db_wise_data'
+    ),
+    url(r'^apps/(?P<app_name>\w+)/$',
+        'django_db_meter.views.list_apps',
+        name='view_app'
+    ),
+    url(r'^apps/$',
+        'django_db_meter.views.list_apps',
+        name='list_apps'
+    ),
+    url(r'^tables/(?P<table_name>\w+)/$',
+        'django_db_meter.views.list_tables',
+        name='view_table'
+    ),
+    url(r'^tables/$',
+        'django_db_meter.views.list_tables',
+        name='list_tables'
+    ),
+    url(r'^dbs/(?P<db_name>\w+)/$',
+        'django_db_meter.views.list_dbs',
+        name='view_db'
+    ),
+    url(r'^dbs/$',
+        'django_db_meter.views.list_dbs',
+        name='list_dbs'
+    ),
 ]
--- django_db_meter/views.py
+++ django_db_meter/views.py
@@ -1,6 +1,10 @@
 import json
 
+from django.conf import settings
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
+from django.db.models import get_apps, get_models
+from django.shortcuts import render
 
 from django_db_meter.models import AppWiseAggregatedMetric
 from django_db_meter.models import TableWiseAggregatedMetric
@@ -8,8 +12,50 @@ from django_db_meter.models import DBWiseAggregatedMetric
 
 # Create your views here.
 
+def index(request):
+    ctx = {}
+    template = 'index.html'
+    return render(request, template, ctx)
+
+def list_apps(request, app_name=None):
+    template = 'index.html'
+    if app_name is None:
+        ctx = {
+            'apps': [app.__package__.split('.')[-1] for app in get_apps()]
+        }
+    else:
+        ctx = {'url': reverse('app_wise_data', args=(app_name,))}
+    return render(request, template, ctx)
+
+def list_tables(request, table_name=None):
+    template = 'index.html'
+    if table_name is None:
+        tables = []
+        for app in get_apps():
+            for model in get_models(app):
+                tables.append(model._meta.db_table)
+        ctx = {
+            'tables': tables
+        }
+    else:
+        ctx = {'url': reverse('table_wise_data', args=(table_name,))}
+    return render(request, template, ctx)
+
+def list_dbs(request, db_name=None):
+    template = 'index.html'
+    if db_name is None:
+        dbs = [db.get('NAME') for db in settings.DATABASES.itervalues()]
+        ctx = {
+            'dbs': dbs
+        }
+    else:
+        ctx = {
+            'url': reverse('db_wise_data', args=(db_name,))
+        }
+    return render(request, template, ctx)
 
 def get_app_wise_data(request, app_name):
+
     data = AppWiseAggregatedMetric.objects.filter(
             app_name=app_name).order_by('-timestamp')[:40]
     response = [{
@@ -24,6 +70,7 @@ def get_app_wise_data(request, app_name):
     return response
 
 def get_table_wise_data(request, table_name):
+
     data = TableWiseAggregatedMetric.objects.filter(
             table_name=table_name).order_by('-timestamp')[:40]
     response = [{
